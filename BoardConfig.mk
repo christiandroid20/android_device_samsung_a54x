2e54ceb5c82f28a80a7f905e3e0609898dbab781
Christy Vela <75439616+christiandroid20@users.noreply.github.com>
2026-06-08 08:27:26 -0600

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -41,7 +41,7 @@ BOARD_KERNEL_CMDLINE := \
     loop.max_part=7
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_MKBOOTIMG_ARGS := \
-    --dtb.img $(DEVICE_PATH)/prebuilt/dtb \
+    --dtb $(DEVICE_PATH)/prebuilt/dtbo.img \
     --ramdisk_offset 0 \
     --dtb_offset 0 \
     --os_version 13.0.0 \
